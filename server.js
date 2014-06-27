5e1af75a7329f9fc14fa48681c8534a36d3f1974
Kevin Ferri <kevinferri@gmail.com>
2014-06-27 16:22:37 -0400

    Setting up the node server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var mime = require('mime');
 var cache = {};
 
-function send404(respone) {
+function send404(response) {
 	response.writeHead(404, {'Content-type': 'text/plain'});
 	response.write('Error 404: reseouce not found.');
 	response.end();
@@ -18,3 +18,40 @@ function sendFile(response, filePath, fileContents) {
 	response.end(fileContents);
 }
 
+function serveStatic(response, cache, absPath) {
+	if (cache[absPath]) {
+		sendFile(response, absPath, cache[absPath]);
+	} else {
+		fs.exists(absPath, function(exists) {
+			if (exists) {
+				fs.readFile(absPath, function(err, data) {
+					if (err) {
+						send404(response);
+					} else {
+						cache[absPath] = data;
+						sendFile(response, absPath, data);
+					}
+				});
+			} else {
+				send404(response);
+			}
+		});
+	}
+}
+
+var server = http.createServer(function(request, response) {
+	var filePath = false;
+
+	if (request.url == '/') {
+		filePath = 'public/index.html';
+	} else {
+		filePath = 'public' + request.url;
+	}
+
+	var absPath = './' + filePath;
+	serveStatic(response, cache, absPath);
+});
+
+server.listen(3000, function() {
+	console.log("Server listening on port 3000");
+});
