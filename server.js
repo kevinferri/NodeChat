326560041d593feb4ef8db3065685a6719010aab
Kevin Ferri <kevinferri@Kevins-MacBook-Pro.local>
2014-07-22 01:29:10 -0400

    Fixed styling

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ function send404(response) {
 }
 
 function sendFile(response, filePath, fileContents) {
-  response.writeHead(
-  200,
-  {"content-type": mime.lookup(path.basename(filePath))}
-  );
+  response.writeHead(200, {"content-type": mime.lookup(path.basename(filePath))});
   response.end(fileContents);
 }
 
